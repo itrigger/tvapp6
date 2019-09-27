59b89a00684efc6febb4c164b6c284d38d550f5d
DESKTOP-N9C8HF0\Konstantin <trigger.kst@gmail.com>
2019-09-28 02:56:54 +0600

    6

--- frontend/src/components/Slides/SlideTableItem.jsx
+++ frontend/src/components/Slides/SlideTableItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Parser from 'html-react-parser';
+import {Button} from "react-bootstrap";
 
 const SlideTableItem = (props) => {
     let path = "/slideupdate/"+props.id;
@@ -17,7 +18,7 @@ const SlideTableItem = (props) => {
                 <div className="float-left m-1"><a href={path} className="btn btn-warning"><FontAwesomeIcon icon={faEdit} /></a></div>
                 <form className="slide-delete-form float-left m-1">
                     <input type="hidden" className="id" value={props.id} />
-                        <a href="#" className="btn btn-danger"><FontAwesomeIcon icon={faTrash} /></a>
+                        <Button variant="danger"><FontAwesomeIcon icon={faTrash} /></Button>
                 </form>
             </td>
         </tr>
