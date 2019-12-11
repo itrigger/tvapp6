a472d80ab8779fc211d84694ce4ee90391a1c857
Konstantin <trigger.kst@gmail.com>
2019-12-11 18:52:09 +0600

    bug fix

--- frontend/src/components/Play/Play.jsx
+++ frontend/src/components/Play/Play.jsx
@@ -1,84 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Parser from "html-react-parser";
+/*import $ from 'jquery';*/
 import OwlCarousel from 'react-owl-carousel2';
 import './../../assets/css/owl.css';
-import $ from 'jquery';
-import Swiper from 'react-id-swiper';
-import 'swiper/css/swiper.css';
 
 
-const Play =(props)=>{
+class Play extends React.Component{
 
 
-        const [swiper, updateSwiper] = useState(null);
-
-        const goNext = () => {
-            if (swiper !== null) {
-                swiper.slideNext();
-            }
+    render() {
+        const options = {
+            items: 1,
+            nav: false,
+            rewind: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            animateOut: 'fadeOut'
         };
-
-        const goPrev = () => {
-            if (swiper !== null) {
-                swiper.slidePrev();
-            }
-        };
-
-
-    const params = {
-        Swiper,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        effect: 'fade',
-        loop: true,
-        observer: true,
-        on: {
-            init: function () {
-                console.log('init');
-               setTimeout(Swiper.slideNext(),$(".swiper-slide-active").data('delay'));
-            },
-            slideChange: function () {
-                console.log('rotateSlide');
-            }
-        },
-    }
-
-
-        /*
 
         const events = {
             onTranslated: function () {
-                console.log('rotateNext');
+               /* console.log('rotateNext');
                 console.log($(".owl-item.active .item").data('delay'));
-                setTimeout(()=>$(".clicknext").click(),$(".owl-item.active .item").data('delay'));
+                setTimeout(()=>$(".clicknext").click(),$(".owl-item.active .item").data('delay'));*/
             },
             onInitialized: function () {
-                clearTimeout(startedTO);
-                clearTimeout(intervalTO);
-                console.log('rotateSlide');
+                /*console.log('rotateSlide');
                 console.log($(".owl-item:first-child .item").data('delay'));
-                setTimeout(()=>$(".clicknext").click(),$(".owl-item:first-child .item").data('delay'));
+                setTimeout(()=>$(".clicknext").click(),$(".owl-item:first-child .item").data('delay'));*/
             }
         };
-*/
-
-
         return (
             <div className={"fullscreen"}>
-                {props.slides.length>0?
-                    <Swiper {...params}>
-                        {props.slides.map(e =>
+                {this.props.slides.length>0?
+                    <OwlCarousel ref="car" options={options} events={events}>
+                        {this.props.slides.map(e =>
                             <div className="item" data-delay={e.delay} key={e._id}>{!e.slide_content || null ? '' : Parser(e.slide_content)}</div>
                         )}
-                    </Swiper>
+                    </OwlCarousel>
                     :""}
-                <button onClick={goPrev}>Prev</button>
-                <button onClick={goNext}>Next</button>
+               {/* <button className={"clicknext"} onClick={() => this.refs.car.next()}>Next</button>*/}
             </div>
         )
-
+    }
 }
 
 export default Play;
