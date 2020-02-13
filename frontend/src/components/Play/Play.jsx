68edd3c0e673e234844b624806a897bb18b5d3c1
Konstantin <trigger.kst@gmail.com>
2020-02-13 18:55:43 +0600

    Backend and Frontend Auth reworked. Data and token saves in localstorage on the frontend and now we can reload f5 front and dont loose session

--- frontend/src/components/Play/Play.jsx
+++ frontend/src/components/Play/Play.jsx
@@ -1,45 +1,42 @@
 import React from 'react';
 import Parser from "html-react-parser";
-/*import $ from 'jquery';*/
-import OwlCarousel from 'react-owl-carousel2';
-import './../../assets/css/owl.css';
+import $ from 'jquery';
 
 
-class Play extends React.Component{
 
+
+class Play extends React.Component {
 
     render() {
-        const options = {
-            items: 1,
-            nav: false,
-            rewind: true,
-            autoplay: true,
-            autoplayTimeout: 3000,
-            animateOut: 'fadeOut'
-        };
+        let t;
 
-        const events = {
-            onTranslated: function () {
-               /* console.log('rotateNext');
-                console.log($(".owl-item.active .item").data('delay'));
-                setTimeout(()=>$(".clicknext").click(),$(".owl-item.active .item").data('delay'));*/
-            },
-            onInitialized: function () {
-                /*console.log('rotateSlide');
-                console.log($(".owl-item:first-child .item").data('delay'));
-                setTimeout(()=>$(".clicknext").click(),$(".owl-item:first-child .item").data('delay'));*/
-            }
-        };
+        let start = $('#myCarousel').find('.active').attr('data-interval');
+        t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
+        $('#myCarousel').on('slid.bs.carousel', function () {
+            console.log('asdfsdf');
+            clearTimeout(t);
+            let duration = $(this).find('.active').attr('data-interval');
+
+            $('#myCarousel').carousel('pause');
+            t = setTimeout("$('#myCarousel').carousel();", duration-1000);
+        });
+
         return (
             <div className={"fullscreen"}>
-                {this.props.slides.length>0?
-                    <OwlCarousel ref="car" options={options} events={events}>
-                        {this.props.slides.map(e =>
-                            <div className="item" data-delay={e.delay} key={e._id}>{!e.slide_content || null ? '' : Parser(e.slide_content)}</div>
-                        )}
-                    </OwlCarousel>
-                    :""}
-               {/* <button className={"clicknext"} onClick={() => this.refs.car.next()}>Next</button>*/}
+                {this.props.slides.length > 0 ?
+                    <div id="myCarousel" className="carousel slide">
+                        <div className="carousel-inner">
+                            {this.props.slides.map(e =>
+                                <div className="item" data-interval={e.delay}
+                                     key={e._id}>{!e.slide_content || null ? '' : Parser(e.slide_content)}</div>
+                            )}
+                        </div>
+
+                        <a className="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                        <a className="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                    </div>
+                    : ""}
+
             </div>
         )
     }
