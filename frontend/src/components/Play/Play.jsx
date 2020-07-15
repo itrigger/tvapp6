01505082e029058c975a5d48ad5bcc836a818ca3
DESKTOP-N9C8HF0\Konstantin <trigger.kst@gmail.com>
2020-07-16 01:23:49 +0600

    Play carousel version 2 w react slick slider (done!)

--- frontend/src/components/Play/Play.jsx
+++ frontend/src/components/Play/Play.jsx
@@ -1,56 +1,57 @@
 import React, {Component} from 'react'
 import Parser from "html-react-parser"
-import Carousel from 'react-elastic-carousel'
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 class Play extends Component {
-    componentDidMount() {
-
-        let carItem = document.getElementsByClassName('item');
-        console.log(carItem)
-        for (let i = 0; i < carItem.length; i++) {
-            console.log(carItem.item(i).innerHTML);
-        }
-    }
+    /*function which toggle next slide by interval receiving from data attr*/
+    nextSlideByTimer = (current) => {
+        let t;
+        let start = this.slider.props.children[current].props["data-interval"];
+        this.slider.slickPause();
+        clearTimeout(t);
+        t = setTimeout(() => {
+            /*need stop timeout when unmount component, dont know how, this is my solution*/
+            if (this.slider != null) {
+                this.slider.slickNext()
+            } else {
+                clearTimeout(t);
+            }
+        }, start);
+    };
 
     render() {
-
- /*       let start = $('#myCarousel').find('.active').attr('data-interval');
-        t = setTimeout("$('#myCarousel').carousel({interval: 1000});", start-1000);
-        $('#myCarousel').on('slid.bs.carousel', function () {
-            console.log('asdfsdf');
-            clearTimeout(t);
-            let duration = $(this).find('.active').attr('data-interval');
-
-            $('#myCarousel').carousel('pause');
-            t = setTimeout("$('#myCarousel').carousel();", duration-1000);
-        });*/
-
-
+        const settings = {
+            dots: false,
+            arrows: false,
+            pauseOnHover: false,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            autoplay: true,
+            afterChange: (current) => this.nextSlideByTimer(current)
+        };
 
         return (
             <div className={"fullscreen"} id={"myCarousel"}>
                 {this.props.slides.length > 0 ?
                     <div>
-                        <button onClick={() => this.carousel.slideNext()}>Next</button>
-                        <hr/>
-                        <Carousel itemsToShow={1}
-                                  enableAutoPlay
-                                  ref={ref => (this.carousel = ref)}
-                                  pagination={false}
-                                  showArrows={false}
-                                  onChange={(currentItem, pageIndex) =>
-                                      console.log(pageIndex)
-                                  }
+                        <Slider
+                            ref={c => (this.slider = c)}
+                            {...settings}
                         >
                             {this.props.slides.map((e, index) =>
-                                <div key={e._id} className={index === 0 ? 'item active' : 'item'} data-interval={e.delay}
+                                <div key={e._id} className={index === 0 ? 'item active' : 'item'}
+                                     data-interval={e.delay}
                                 >{!e.slide_content || null ? '' : Parser(e.slide_content)}</div>
                             )}
-                        </Carousel>
+                        </Slider>
                     </div>
                     : ""}
             </div>
+
         )
     }
 }
